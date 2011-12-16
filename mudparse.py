de1f053ca5fafc1174e2a023a62a7d54478ed929
Benjamin Pollack <benjamin@fogcreek.com>
2011-12-16 12:37:29 -0500

    fix whitespace

--- mudparse.py
+++ mudparse.py
@@ -3,32 +3,32 @@ from flask import session
 CONFUSED = u"Not sure what you meant."
 
 def interpret(scmd):
-	scmd = scmd.lower()
-	parts = scmd.split()
-	if parts[0] == "help":
-		return helpverb(parts[1:])
-	if parts[0] == "move" or parts[0] == "go":
-		return move(parts[1])
-	
-	return CONFUSED
+    scmd = scmd.lower()
+    parts = scmd.split()
+    if parts[0] == "help":
+        return helpverb(parts[1:])
+    if parts[0] == "move" or parts[0] == "go":
+        return move(parts[1])
+
+    return CONFUSED
 
 def helpverb(expr):
-	return "MOVE or GO: NORTH, EAST, SOUTH, WEST (UP, RIGHT, DOWN, LEFT)"
+    return "MOVE or GO: NORTH, EAST, SOUTH, WEST (UP, RIGHT, DOWN, LEFT)"
 
 def move(sdir):
-	loc = str.split(session['room'], '_')
-	x = int(loc[0])
-	y = int(loc[1])
-	if sdir in ("left", "west"):
-		x -= 1
-	elif sdir in ("right", "east"):
-		x += 1
-	elif sdir in ("up", "north"):
-		y += 1
-	elif sdir in ("down", "south"):
-		y -= 1
-	else:
-		return CONFUSED
+    loc = str.split(session['room'], '_')
+    x = int(loc[0])
+    y = int(loc[1])
+    if sdir in ("left", "west"):
+        x -= 1
+    elif sdir in ("right", "east"):
+        x += 1
+    elif sdir in ("up", "north"):
+        y += 1
+    elif sdir in ("down", "south"):
+        y -= 1
+    else:
+        return CONFUSED
 
-	session['room'] = str(x) + "_" + str(y)
-	return "You are in room " + session['room']
+    session['room'] = str(x) + "_" + str(y)
+    return "You are in room " + session['room']
