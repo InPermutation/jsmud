6b21c53e445aa0a8f7199a859b6f1e0d5cdf234f
Benjamin Pollack <benjamin@fogcreek.com>
2011-12-16 12:40:53 -0500

    clean up variable names and logic

--- mudparse.py
+++ mudparse.py
@@ -2,30 +2,27 @@ from flask import session
 
 CONFUSED = u"Not sure what you meant."
 
-def interpret(scmd):
-    scmd = scmd.lower()
-    parts = scmd.split()
-    if parts[0] == "help":
-        return helpverb(parts[1:])
-    if parts[0] == "move" or parts[0] == "go":
-        return move(parts[1])
+def interpret(line):
+    command, args = line.lower().split(maxsplit=1)
+    if command == "help":
+        return helpverb(args.split())
+    if command in ('move', 'go'):
+        return move(args.split()[0])
 
     return CONFUSED
 
 def helpverb(expr):
     return "MOVE or GO: NORTH, EAST, SOUTH, WEST (UP, RIGHT, DOWN, LEFT)"
 
-def move(sdir):
-    loc = str.split(session['room'], '_')
-    x = int(loc[0])
-    y = int(loc[1])
-    if sdir in ("left", "west"):
+def move(direction):
+    x, y = [int(loc) for loc in session['room'].split('_')]
+    if direction in ("left", "west"):
         x -= 1
-    elif sdir in ("right", "east"):
+    elif direction in ("right", "east"):
         x += 1
-    elif sdir in ("up", "north"):
+    elif direction in ("up", "north"):
         y += 1
-    elif sdir in ("down", "south"):
+    elif direction in ("down", "south"):
         y -= 1
     else:
         return CONFUSED
